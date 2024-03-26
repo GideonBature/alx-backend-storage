5f23cafab1e6e2b7f9004332fb55005d18c9f28a
GideonBature <infoaboutgideon@gmail.com>
2024-03-26 03:45:25 +0100

    code won't run immediately it is imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,18 +2,20 @@
 """12 Log stats
 """
 from pymongo import MongoClient
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.logs
-nginx_collection = db.nginx
 
-print(f"{nginx_collection.count_documents({})} logs")
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
 
-print(f"Methods:")
+    print(f"{nginx_collection.count_documents({})} logs")
 
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = nginx_collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
+    print(f"Methods:")
 
-method_get_count = nginx_collection.count_documents({"method": "GET",
-                                                  "path": "/status"})
-print(f"{method_get_count} status check")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    method_get_count = nginx_collection.count_documents({"method": "GET",
+                                                         "path": "/status"})
+    print(f"{method_get_count} status check")
