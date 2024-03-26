a6a8175a172fdeab3cb1a7de52ce329fa1c6641b
GideonBature <infoaboutgideon@gmail.com>
2024-03-26 03:29:00 +0100

    change to use normal import against __import__

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 """12 Log stats
 """
+from pymongo import MongoClient
+client = MongoClient('mongodb://127.0.0.1:27017')
+db = client.logs
+nginx_collection = db.nginx
 
-
-db_collection = __import__('pymongo').MongoClient().logs.nginx
-
-print(f"{db_collection.count_documents({})} logs")
+print(f"{nginx_collection.count_documents({})} logs")
 
 print(f"Methods:")
 
 for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = db_collection.count_documents({"method": method})
+    count = nginx_collection.count_documents({"method": method})
     print(f"\tmethod {method}: {count}")
 
-method_get_count = db_collection.count_documents({"method": "GET",
+method_get_count = nginx_collection.count_documents({"method": "GET",
                                                   "path": "/status"})
 print(f"{method_get_count} status check")
